f5f9b5360c28e76cfbbe2ae76ac1c159e1f90f9e
Martin Carolan <martin@typesystems.co.uk>
2023-04-10 15:35:54 +0800

    safer deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,31 @@
 set -e
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
+cowsay fetching
+git fetch
 hugo --gc --minify
 
+if ! git diff-files --quiet; then
+  cowsay "you have untracked files bro"
+  exit 1
+fi
+
+BEHIND=$(git rev-list @..@{u} --count)
+if ! echo -n $BEHIND | grep '^0$' > /dev/null; then
+  cowsay "${BEHIND} behind remote, plz pull!"
+  exit 1
+fi
+
+UNPUSHED=$(git rev-list @{u}..@ --count)
+if ! echo -n $UNPUSHED | grep '^0$' > /dev/null; then
+  cowsay "${UNPUSHED} infront of remote, plz push!"
+  exit 1
+fi
+
+if ! git status --porcelain; then
+  cowsay "uncommited  changes!"
+  exit 1
+fi
+
 cowsay syncing site
-rclone sync --verbose "$SCRIPTPATH"/public vps:/var/www/vps
+rclone sync --verbose "$SCRIPTPATH"/public vps:/var/www/root
